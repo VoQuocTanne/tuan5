873467a61f39cc77edcbd1f675fcae9be354b083
VoQuocTanne <voquoctan003@gmail.com>
2024-06-05 11:28:53 +0700

    zz

---
+++ src/test/java/com/hutech/Buoi222/Buoi222ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hutech.Buoi222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buoi222ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
